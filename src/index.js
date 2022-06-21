888225576fb52485dfa83632d14cc1b6d0364bee
Graycemuthui <graycemuthui20@gmail.com>
2022-06-21 12:28:26 +0300

    Create the footer section

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import _ from "lodash";
 import "./styles.css";
 
-import Movies from "./showmovies";
+import Movies from "../src/modules/items.js";
 Movies.displayMovies();
 // fetchApi.getLikes();
 Movies.counterMovies();
