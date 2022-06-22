1602166a1a828fb0b1632dc87c522edb0df0e2c0
jheart-vic <adebowalevictorjheart.com>
2022-06-22 23:11:24 +0100

    Fix linters error

--- test/items.test.js
+++ test/items.test.js
@@ -1,6 +1,6 @@
-import Movies from "../modules/items";
+import Movies from '../modules/items.js';
 
-test("MoviesCounter test", async () => {
+test('MoviesCounter test', async () => {
   const counter = await Movies.counterMovies();
   expect(counter).toBe(9);
 });
