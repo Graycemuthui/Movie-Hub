60137356a0e861c2722f0208bba16ba747432111
Graycemuthui <graycemuthui20@gmail.com>
2022-06-23 03:30:51 +0300

    Fix linters

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import "./styles.css";
+import './styles.css';
 
-import LikesApi from "./likesApi";
-import Movies from "../modules/items";
+import LikesApi from './likesApi';
+import Movies from '../modules/items';
 
 Movies.displayMovies();
 Movies.counterMovies();
